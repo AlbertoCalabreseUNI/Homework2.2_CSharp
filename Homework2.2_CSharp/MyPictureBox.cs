2ac84f7088cb02b7a8905c201cb346ad686d9151
Alberto Calabrese <calabrese.1709532@studenti.uniroma1.it>
2021-10-08 13:37:18 +0200

    Added resizable image at runtime.

--- Homework2.2_CSharp/MyPictureBox.cs
+++ Homework2.2_CSharp/MyPictureBox.cs
@@ -1,20 +1,37 @@
 ﻿using System.Windows.Forms;
 using System.ComponentModel;
 using System.Drawing;
+using System;
 
 namespace Homework2._2_CSharp
 {
     class MyPictureBox : PictureBox
     {
         Point point;
-        public MyPictureBox(IContainer container)
+        private bool MouseIsInLeftEdge;
+        private bool MouseIsInRightEdge;
+        private bool MouseIsInTopEdge;
+        private bool MouseIsInBottomEdge;
+
+		private int currentHeight;
+		private int currentWidth;
+
+		public MyPictureBox(IContainer container)
         {
-            container.Add(this);
+			container.Add(this);
         }
 
         protected override void OnMouseDown(MouseEventArgs e)
         {
             point = e.Location;
+            MouseIsInLeftEdge = Math.Abs(point.X) <= 10;
+            MouseIsInRightEdge = Math.Abs(point.X - this.Width) <= 10;
+            MouseIsInTopEdge = Math.Abs(point.Y) <= 10;
+            MouseIsInBottomEdge = Math.Abs(point.Y - this.Height) <= 10;
+
+			currentHeight = this.Height;
+			currentWidth = this.Width;
+
             base.OnMouseDown(e);
         }
 
@@ -22,12 +39,62 @@ namespace Homework2._2_CSharp
         {
             if (e.Button == MouseButtons.Left)
             {
-                this.Left += e.X - point.X;
-                this.Top += e.Y - point.Y;
-            }
-            
+				if (MouseIsInLeftEdge)
+				{
+					if (MouseIsInTopEdge)
+					{
+						this.Width -= (e.X - point.X);
+						this.Left += (e.X - point.X);
+						this.Height -= (e.Y - point.Y);
+						this.Top += (e.Y - point.Y);
+					}
+					else if (MouseIsInBottomEdge)
+					{
+						this.Width -= (e.X - point.X);
+						this.Left += (e.X - point.X);
+						this.Height = (e.Y - point.Y) + this.currentHeight;
+					}
+					else
+                    {
+						this.Width -= (e.X - point.X);
+						this.Left += (e.X - point.X);
+					}
+				}
+				else if (MouseIsInRightEdge)
+				{
+					if (MouseIsInTopEdge)
+					{
+						this.Width = (e.X - point.X) + this.currentWidth;
+						this.Height -= (e.Y - point.Y);
+						this.Top += (e.Y - point.Y);
+
+					}
+					else if (MouseIsInBottomEdge)
+					{
+						this.Width = (e.X - point.X) + this.currentWidth;
+						this.Height = (e.Y - point.Y) + this.currentHeight;
+					}
+                    else
+                    {
+						this.Width = (e.X - point.X) + this.currentWidth;
+					}
+				}
+				else if (MouseIsInTopEdge)
+				{
+					this.Height -= (e.Y - point.Y);
+					this.Top += (e.Y - point.Y);
+				}
+				else if (MouseIsInBottomEdge)
+				{
+					this.Height = (e.Y - point.Y) + this.currentHeight;
+				}
+				else
+				{
+					this.Left += e.X - point.X;
+					this.Top += e.Y - point.Y;
+				}
+			}
             base.OnMouseMove(e);
         }
-
     }
 }
