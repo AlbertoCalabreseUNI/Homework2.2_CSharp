1e62d505b5008c20ecddf1c8d6493213e0380e85
Università <Universit@DESKTOP-4V098K9>
2021-10-08 10:51:55 +0200

    Add project files.

---
+++ Homework2.2_CSharp/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Homework2._2_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.myPictureBox1 = new Homework2._2_CSharp.MyPictureBox(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myPictureBox1
+            // 
+            this.myPictureBox1.Image = global::Homework2._2_CSharp.Properties.Resources.cat;
+            this.myPictureBox1.Location = new System.Drawing.Point(94, 35);
+            this.myPictureBox1.Name = "myPictureBox1";
+            this.myPictureBox1.Size = new System.Drawing.Size(261, 294);
+            this.myPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myPictureBox1.TabIndex = 0;
+            this.myPictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.myPictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.myPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyPictureBox myPictureBox1;
+    }
+}
+
---
+++ Homework2.2_CSharp/MyPictureBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace Homework2._2_CSharp
+{
+    class MyPictureBox : PictureBox
+    {
+        Point point;
+        public MyPictureBox(IContainer container)
+        {
+            container.Add(this);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            point = e.Location;
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - point.X;
+                this.Top += e.Y - point.Y;
+            }
+            
+            base.OnMouseMove(e);
+        }
+
+    }
+}
